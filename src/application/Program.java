b913e6e63636e25b15c83ef7690d32ae7c5d965b
natalia0412 <natalia.vieiramonteiro70@gmail.com>
2022-11-17 15:58:51 -0300

    criação da class BOard e Pieace

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
         Position ps=new Position(3,5);
         System.out.println(ps);
+        Board board =new Board(8,8);
+
     }
 }
