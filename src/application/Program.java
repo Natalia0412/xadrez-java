823511665e9a4af9c29373e8bb23368542593b75
natalia0412 <natalia.vieiramonteiro70@gmail.com>
2022-11-19 20:09:41 -0300

    implementação da chess layer e print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,12 @@ package application;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Position ps=new Position(3,5);
-        System.out.println(ps);
-        Board board =new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
